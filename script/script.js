f8bfec4bfebdf99f4b08114aa0af1a295801ece4
Entropie67 <olivier.elophe@gmail.com>
2023-12-01 12:02:51 +0100

    chiffrement effectif

--- script/script.js
+++ script/script.js
@@ -4,6 +4,20 @@ chiffrer.addEventListener("click", () => {
     let message = document.getElementById("message").value;
     let cle = document.getElementById("cle").value;
     let resultat = document.getElementById("resultat");
-    resultat.innerHTML = "Message : " + message + "<br/>Clé : " + cle;
+    resultat.innerHTML = "Message chiffré : " + cesar(message, cle);
 }); 
 
+let cesar = (message, cle) => {
+    let resultat = "";
+    for (let i = 0; i < message.length; i++) {
+        let code = message.charCodeAt(i);
+        if (code >= 65 && code <= 90) {
+            resultat += String.fromCharCode((code - 65 + cle) % 26 + 65);
+        } else if (code >= 97 && code <= 122) {
+            resultat += String.fromCharCode((code - 97 + cle) % 26 + 97);
+        } else {
+            resultat += message.charAt(i);
+        }
+    }
+    return resultat;
+}
